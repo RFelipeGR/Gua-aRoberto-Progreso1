c330140906e0e6163da1a13cad4a2b6ce6375f9b
Roberto Guaña <robertofelipe20.rf@gmail.com>
2024-10-29 08:30:54 -0500

    commit 1432

---
+++ GuañaRobertoProgreso1/Models/Celular.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuañaRobertoProgreso1.Models
+{
+    public class Celular
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Modelo es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo no puede exceder 100 caracteres.")]
+        public string Modelo { get; set; }
+
+        [Range(2000, 2100, ErrorMessage = "El año debe estar entre 2000 y 2100.")]
+        public int Año { get; set; }
+
+        [Range(0.0, Double.MaxValue, ErrorMessage = "El precio debe ser mayor o igual a 0.")]
+        public decimal Precio { get; set; }
+
+       
+        public int RGuañaId { get; set; }
+        public RGuaña RGuaña { get; set; }
+
+    }
+}
---
+++ GuañaRobertoProgreso1/Models/RGuaña.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace GuañaRobertoProgreso1.Models
+{
+    
+    public class RGuaña
+    {
+        public ICollection<Celular> Celulares { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo AtributoInt es obligatorio.")]
+        public int AtributoInt { get; set; }
+
+        [Range(0.0, Double.MaxValue, ErrorMessage = "El precio debe ser mayor o igual a 0.")]
+        public decimal AtributoDecimal { get; set; }
+
+        [StringLength(100, ErrorMessage = "El campo no puede exceder 100 caracteres.")]
+        public string AtributoString { get; set; }
+
+        public bool AtributoBool { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "El campo AtributoFecha es obligatorio.")]
+        public DateTime AtributoFecha { get; set; }
+
+    }
+}
